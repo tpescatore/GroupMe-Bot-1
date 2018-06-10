7baee6bfe36f54fcce5dff6e7f5cbf94237cd633
cvirostek <cwvirostek@gmail.com>
2018-06-10 09:09:15 +0000

    Added option to set "dadness"
    Dadness is the chance that the bot wil tell dad jokes

--- reactions.js
+++ reactions.js
@@ -1,5 +1,6 @@
 var groupme = require("groupme");
 var fs = require("fs");
+var util = require("./util");
 
 var config = JSON.parse(fs.readFileSync("config.json")); 
 
@@ -7,15 +8,48 @@ var wolfram = require("wolfram-alpha").createClient(config.WA_key);
 
 var reactions = {};
 
+function init_reactions() {
+	var obj;
+	var data = fs.readFileSync("data.json", {flag: "a+"});
+
+	// Load data.json, using empty object if it's empty
+	if (data.length == 0) {
+		obj = {};
+	}
+	else {
+		try {
+			obj = JSON.parse(data);
+		}
+		catch (e) {
+			console.log("Error parsing data.json");
+			return;
+		}
+	}
+
+	for (var i in reactions) {
+		if (reactions[i].init) {
+			reactions[i].init(obj);
+		}
+	}
+}
+
 reactions.dadjoke = {
 	// Check for any occurrence of "I'm ____"
 	re1: /(?:^|\s)(?:i[‘’`']?m|i am)\s+(\w+)(?:\W)?/i, // Match a single word
 	re: /(?:^|\s)(?:i[‘’`']?m|i am) +([^.,?!:;\n]+?)\s*(?:\.|,|\?|!|:|;|\n|$)/i, // Match an entire phrase
+	match: null,
+	prob: 0.2,
+	init: function(data) {
+		if (data.dadness) {
+			this.prob = data.dadness;
+		}
+	},
 	check: function(msg) {
-		return this.re.test(msg.text);
+		this.match = this.re.exec(msg.text);
+		return this.match && Math.random() < this.prob;
 	},
 	reply: function(msg) {
-		var name = this.re.exec(msg.text)[1];
+		var name = this.match[1];
 		// Specific message for a specific user
 		if (msg.user_id == "23766274") {
 			var joke = "Hi " + name + ", I'm da\uD83C\uDD71\uFE0F!";
@@ -31,9 +65,45 @@ reactions.dadjoke = {
 	}
 }
 
+reactions.dadness = {
+	re: /!dadness\s*(.*)?/i,
+	match: null,
+	check: function(msg) {
+		this.match = this.re.exec(msg.text);
+		return this.match;
+	},
+	reply: function(msg) {
+		var response;
+		var val = this.match[1] && parseFloat(this.match[1]);
+		if (val < 0 || val > 1) {
+			val = undefined;
+		}
+		if (val) {
+			reactions.dadjoke.prob = val;
+			util.store_data("dadness", val);
+			response = "Dadness level set to " + 100 * val + "%.";
+		}
+		else {
+			if (this.match[1]) {
+				response = "Error: dadness level must be between 0 and 1.";
+			}
+			else {
+				response = "Dadness level: " + 100 * reactions.dadjoke.prob + "%.";
+			}
+		}
+		
+		groupme.Stateless.Bots.post("", msg.bot_id, response, {}, function(err, res) {
+			if (err) {
+				console.log(err.statusCode, err.statusMessage);
+			}
+		});
+	}
+}
+
 reactions.factorial = {
 	// Check for any occurrence of a number followed by exclamation mark(s)
 	re: /((?:\d|,|\.)+)(!+)(?:[^!]|$)/,
+	match: null,
 	// Find the evaluated factorial in the Wolfram Alpha response
 	getResults: function(body) {
 		var result = null;
@@ -59,12 +129,12 @@ reactions.factorial = {
 		return [input, result];
 	},
 	check: function(msg) {
-		return this.re.test(msg.text);
+		this.match = this.re.exec(msg.text);
+		return this.match;
 	},
 	reply: function(msg) {
-		var match = this.re.exec(msg.text);
-		var n = match[1];
-		var facts = match[2];
+		var n = this.match[1];
+		var facts = this.match[2];
 
 		// Make sure there's actually a number, not just commas and periods
 		if (!/[0-9]/.test(n)) {
@@ -104,4 +174,5 @@ reactions.factorial = {
 	}
 }
 
+init_reactions();
 module.exports = reactions;
