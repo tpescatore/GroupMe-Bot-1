ea8fa65e67e1692034a0e86aa7e78cb7468ad6b3
tpescatore <tommypescatore@gmail.com>
2019-10-15 15:09:32 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ var config = {}
 config.bot_ids = ["095bd83048b4af78f9aaac090c"];
 config.token = "Los1bE7JCfqfe5OvDJlwYRqu5wLmm6esXiaA6qRm";
 config.callback_url = "/groupme/bot";
-config.WA_key = "XXXXXXXXXXXXXXXXX";
-config.google_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+config.WA_key = "X9UYKPW-23H8T4A94A";
+config.google_key = "AIzaSyBFGpj54zeicmsLnh-_dMyfP9FkPbfGZJk";
 config.port = 3000;
 
 module.exports = config;
