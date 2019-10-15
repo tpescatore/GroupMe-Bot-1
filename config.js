9c6f26f2e30af49faf5329876b4c1bd2c1fc05fa
tpescatore <tommypescatore@gmail.com>
2019-10-15 15:11:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = {}
 
 config.bot_ids = ["095bd83048b4af78f9aaac090c"];
 config.token = "Los1bE7JCfqfe5OvDJlwYRqu5wLmm6esXiaA6qRm";
-config.callback_url = "/groupme/bot";
+config.callback_url = "https://gmmongo.herokuapp.com/?095bd83048b4af78f9aaac090c";
 config.WA_key = "X9UYKPW-23H8T4A94A";
 config.google_key = "AIzaSyBFGpj54zeicmsLnh-_dMyfP9FkPbfGZJk";
 config.port = 3000;
