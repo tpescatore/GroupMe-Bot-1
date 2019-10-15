19f86bfd355d001876542627aa6df575aeddde55
tpescatore <tommypescatore@gmail.com>
2019-10-15 15:08:44 -0400

    Update and rename config.js.example to config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 var config = {}
 
-config.bot_ids = ["XXXXXXXXXXXXXXXXXXXXXXXXXX"];
-config.token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+config.bot_ids = ["095bd83048b4af78f9aaac090c"];
+config.token = "Los1bE7JCfqfe5OvDJlwYRqu5wLmm6esXiaA6qRm";
 config.callback_url = "/groupme/bot";
 config.WA_key = "XXXXXXXXXXXXXXXXX";
 config.google_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
